a05b107d7edbedef5bbd71bd3254dd0f44081238
Alana <alanayee07@gmail.com>
2024-04-26 15:19:16 -0700

    implemented move to next slide feature

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import {BsChevronCompactLeft, BsChevronCompactRight} from 'react-icons/bs'
+import {RxDotFilled} from 'react-icons/rx'
 
 const Featured = () => {
   const sliders = [
@@ -24,6 +25,10 @@ const Featured = () => {
       setCurrentIndex(newIndex);
     }
 
+    const moveToNextSlide = (slideIndex) => {
+      setCurrentIndex(slideIndex)
+    }
+
   return (
     <div className='max-w-[1520px] h-[500px] w-full py-4 px-4 relative group'>
       <div className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
@@ -34,6 +39,15 @@ const Featured = () => {
       </div>
       <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-orange-700 text-white cursor-pointer'>
         <BsChevronCompactRight onClick={nextSlider} />
+      </div>
+      <div className='flex top-4 justify-center py-2'>
+        {
+          sliders.map((sliderItems, slideIndex) => (
+            <div key={slideIndex} className='text-2xl cursor-pointer'>
+              <RxDotFilled onClick={() => moveToNextSlide(slideIndex)}/>
+            </div>
+          ))
+        }
       </div>
     </div>
   )
